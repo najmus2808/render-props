1328cc7cd614eb17491680f5d3d745ce79374d94
najmus2808 <najmus2808@gmail.com>
2021-11-07 00:34:58 +0600

    render props done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from "react";
+import ClickCounter from "./components/ClickCounter";
+import Counter from "./components/Counter";
+import HoverCounter from "./components/HoverCounter";
 
-const App = () => {
-  return <div>Test</div>;
-};
+function App() {
+  return (
+    <div className="app">
+      <Counter>
+        {(counter, incrementCount) => (
+          <ClickCounter count={counter} incrementCount={incrementCount} />
+        )}
+      </Counter>
+      <Counter>
+        {(counter, incrementCount) => (
+          <HoverCounter count={counter} incrementCount={incrementCount} />
+        )}
+      </Counter>
+    </div>
+  );
+}
 
 export default App;
